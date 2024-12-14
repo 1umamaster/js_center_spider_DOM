0542d09df1575b2654eb50731b76573abeef0653
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-14 22:30:15 +0300

    make build

---
+++ index.80dc10da.js
@@ -0,0 +1,2 @@
+var e,t;e=document.querySelector(".wall"),(t=document.querySelector(".spider")).style.top=(e.clientHeight-t.clientHeight)/2+"px",t.style.left=(e.clientWidth-t.clientWidth)/2+"px";
+//# sourceMappingURL=index.80dc10da.js.map
